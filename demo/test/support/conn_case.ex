59ab934340a721c0d8e6f1c77d265ad9606692a9
thanos <thanosv@gmail.com>
2025-11-08 22:21:14 +0000

    add ConnCase test support module

--- demo/test/support/conn_case.ex
+++ demo/test/support/conn_case.ex
@@ -1,4 +1,4 @@
-defmodule ExEditorWeb.ConnCase do
+defmodule DemoWeb.ConnCase do
   @moduledoc """
   This module defines the test case to be used by
   tests that require setting up a connection.
@@ -11,7 +11,7 @@ defmodule ExEditorWeb.ConnCase do
   we enable the SQL sandbox, so changes done to the database
   are reverted at the end of every test. If you are using
   PostgreSQL, you can even run database tests asynchronously
-  by setting `use ExEditorWeb.ConnCase, async: true`, although
+  by setting `use DemoWeb.ConnCase, async: true`, although
   this option is not recommended for other databases.
   """
 
@@ -20,19 +20,19 @@ defmodule ExEditorWeb.ConnCase do
   using do
     quote do
       # The default endpoint for testing
-      @endpoint ExEditorWeb.Endpoint
+      @endpoint DemoWeb.Endpoint
 
-      use ExEditorWeb, :verified_routes
+      use DemoWeb, :verified_routes
 
       # Import conveniences for testing with connections
       import Plug.Conn
       import Phoenix.ConnTest
-      import ExEditorWeb.ConnCase
+      import DemoWeb.ConnCase
     end
   end
 
   setup tags do
-    ExEditor.DataCase.setup_sandbox(tags)
+    Demo.DataCase.setup_sandbox(tags)
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
