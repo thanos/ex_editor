9ca7339adc1df52e2d9c55c7742b264f2fc4c9f4
thanos <thanosv@gmail.com>
2025-11-08 21:39:22 +0000

    Fix syntax error in mix.exs: add comma after credo dep

--- mix.exs
+++ mix.exs
@@ -59,7 +59,7 @@ defmodule ExEditor.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}, # Added missing comma here
       {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
     ]
   end
