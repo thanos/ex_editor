911399d5a7d7e4eba55a7fdc01037008ef6534ea
Fly Dev <dev@fly.io>
2025-11-08 14:14:59 +0000

    replace root route with editor LiveView

--- lib/ex_editor_web/router.ex
+++ lib/ex_editor_web/router.ex
@@ -17,7 +17,7 @@ defmodule ExEditorWeb.Router do
   scope "/", ExEditorWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", EditorLive
   end
 
   # Other scopes may use custom stacks.
