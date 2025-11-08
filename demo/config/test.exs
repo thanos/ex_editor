5741c783f64c19ff9a563659d8324a2df4acbb86
thanos <thanosv@gmail.com>
2025-11-08 22:20:46 +0000

    fix demo test config to use Demo app names

--- demo/config/test.exs
+++ demo/config/test.exs
@@ -5,20 +5,20 @@ import Config
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
-config :ex_editor, ExEditor.Repo,
-  database: Path.expand("../ex_editor_test.db", __DIR__),
+config :demo, Demo.Repo,
+  database: Path.expand("../demo_test.db", __DIR__),
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :ex_editor, ExEditorWeb.Endpoint,
+config :demo, DemoWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "+MQh2S+0ot7/3s+2qwG85eBqIO1mSd1ZpunLopzkPWLs0MKiax1MYuJ3S+tFFU7S",
   server: false
 
 # In test we don't send emails
-config :ex_editor, ExEditor.Mailer, adapter: Swoosh.Adapters.Test
+config :demo, Demo.Mailer, adapter: Swoosh.Adapters.Test
 
 # Disable swoosh api client as it is only required for production adapters
 config :swoosh, :api_client, false
