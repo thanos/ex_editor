f7897615d821008181cb1de7e19515a80b2df4d3
thanos <thanosv@gmail.com>
2025-11-08 21:53:09 +0000

    fix: logger config syntax error

--- demo/config/config.exs
+++ demo/config/config.exs
@@ -52,8 +52,7 @@ config :tailwind,
   ]
 
 # Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\nid]
+config :logger, :console, format: "$time $metadata[$level] $message\n"
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
@@ -61,4 +60,3 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
-
