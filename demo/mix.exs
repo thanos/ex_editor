0b11787642bb082f2a18bbc0ce54f47f25834067
thanos <thanosv@gmail.com>
2025-11-09 14:22:19 +0000

    bumped version

--- demo/mix.exs
+++ demo/mix.exs
@@ -4,7 +4,7 @@ defmodule Demo.MixProject do
   def project do
     [
       app: :demo,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
