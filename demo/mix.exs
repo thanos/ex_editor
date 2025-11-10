c46544693bdb511db9db40c7d62a38a0d308940d
thanos <thanosv@gmail.com>
2025-11-10 23:54:19 +0000

    update dependencies to latest versions

--- demo/mix.exs
+++ demo/mix.exs
@@ -45,7 +45,7 @@ defmodule Demo.MixProject do
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.4.1", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.2.0",
