f4195921f697ea7611871828f7d6dd4679dbd895
thanos <thanosv@gmail.com>
2025-11-10 12:01:39 +0000

    update tests to unwrap new/1 return value

--- test/ex_editor/editor_test.exs
+++ test/ex_editor/editor_test.exs
@@ -5,14 +5,14 @@ defmodule ExEditor.EditorTest do
 
   describe "new/1" do
     test "creates a new editor with an empty document by default" do
-      editor = Editor.new()
+      {:ok, editor} = Editor.new()
       assert %Editor{document: %Document{lines: [""]}} = editor
       assert editor.plugins == []
       assert editor.options == []
     end
 
     test "creates a new editor with provided content" do
-      editor = Editor.new(content: "hello world")
+      {:ok, editor} = Editor.new(content: "hello world")
       assert %Editor{} = editor
       assert ExEditor.Document.to_text(editor.document) == "hello world"
     end
@@ -23,20 +23,20 @@ defmodule ExEditor.EditorTest do
         def on_event(_event, _payload, editor), do: {:ok, editor}
       end
 
-      editor = Editor.new(plugins: [MyPlugin])
+      {:ok, editor} = Editor.new(plugins: [MyPlugin])
       assert editor.plugins == [MyPlugin]
     end
   end
 
   describe "set_content/2" do
     test "sets new content for the editor" do
-      editor = Editor.new(content: "old content")
+      {:ok, editor} = Editor.new(content: "old content")
       {:ok, updated_editor} = Editor.set_content(editor, "new content")
       assert ExEditor.Document.to_text(updated_editor.document) == "new content"
     end
 
     test "returns error for invalid content" do
-      editor = Editor.new()
+      {:ok, editor} = Editor.new()
       {:error, :invalid_content} = Editor.set_content(editor, nil)
     end
 
@@ -59,7 +59,7 @@ defmodule ExEditor.EditorTest do
       # Store test PID in process dictionary for MockPlugin to access
       Process.put(:test_pid, test_pid)
 
-      editor = Editor.new(plugins: [MockPlugin], content: "initial")
+      {:ok, editor} = Editor.new(plugins: [MockPlugin], content: "initial")
       {:ok, _updated_editor} = Editor.set_content(editor, "changed content")
 
       assert_receive {:plugin_event, :handle_change}
@@ -68,12 +68,12 @@ defmodule ExEditor.EditorTest do
 
   describe "get_content/1" do
     test "returns the current content of the editor" do
-      editor = Editor.new(content: "some text")
+      {:ok, editor} = Editor.new(content: "some text")
       assert Editor.get_content(editor) == "some text"
     end
 
     test "returns empty string for new editor" do
-      editor = Editor.new()
+      {:ok, editor} = Editor.new()
       assert Editor.get_content(editor) == ""
     end
   end
