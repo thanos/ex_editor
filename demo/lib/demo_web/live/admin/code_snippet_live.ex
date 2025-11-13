c06f9c98b2528135ad53e807616fc4f507a42a62
thanos <thanosv@gmail.com>
2025-11-13 06:29:14 -0500

    closed #38, closed #39, closed #40

--- demo/lib/demo_web/live/admin/code_snippet_live.ex
+++ demo/lib/demo_web/live/admin/code_snippet_live.ex
@@ -4,15 +4,40 @@ defmodule DemoWeb.Admin.CodeSnippetLive do
         schema: Demo.CMS.CodeSnippet,
         repo: Demo.Repo,
         update_changeset: &Demo.CMS.CodeSnippet.changeset/3,
-        create_changeset: &Demo.CMS.CodeSnippet.changeset/3
+        create_changeset: &Demo.CMS.CodeSnippet.changeset/3,
+        item_query: &__MODULE__.item_query/3
       ],
+      on_mount: {__MODULE__, :local_hook},
       layout: {DemoWeb.Layouts, :admin}
+
+    import Ecto.Query, only: [from: 2]
+
+
+    def on_mount(:local_hook, _params, _session, socket) do
+      {:ok, editor} = ExEditor.Editor.new(content: "")
+      editor = ExEditor.Editor.set_highlighter(editor, ExEditor.Highlighters.Elixir)
+
+      {:cont,
+       socket
+       |> assign(:editor, editor)
+       |> assign(:cursor_line, 1)
+       |> assign(:cursor_col, 1)}
+    end
 
     @impl Backpex.LiveResource
     def singular_name, do: "Code Snippet"
 
     @impl Backpex.LiveResource
     def plural_name, do: "Code Snippets"
+
+
+    def item_query(query, :edit,  assigns), do: item_query(query, :show, assigns)
+    def item_query(query, :show,  %{params: %{"backpex_id" => id}}) do
+      query = from c0 in Demo.CMS.CodeSnippet, as: :codesnippet, where: c0.id == ^id,   select: c0
+      query
+    end
+
+    def item_query(query, _,  _assigns),  do: query
 
     @impl Backpex.LiveResource
     def fields do
@@ -22,7 +47,7 @@ defmodule DemoWeb.Admin.CodeSnippetLive do
           label: "Name"
         },
         code: %{
-          module: Backpex.Fields.Textarea,
+          module: DemoWeb.Admin.Fields.EditField,
           rows: 10,
           label: "Code"
         }
---
+++ demo/lib/demo_web/live/admin/fields/edit_field.ex
@@ -0,0 +1,75 @@
+defmodule DemoWeb.Admin.Fields.EditField do
+    @config_schema [
+      placeholder: [
+        doc: "Placeholder value or function that receives the assigns.",
+        type: {:or, [:string, {:fun, 1}]}
+      ],
+      debounce: [
+        doc: "Timeout value (in milliseconds), \"blur\" or function that receives the assigns.",
+        type: {:or, [:pos_integer, :string, {:fun, 1}]}
+      ],
+      throttle: [
+        doc: "Timeout value (in milliseconds) or function that receives the assigns.",
+        type: {:or, [:pos_integer, {:fun, 1}]}
+      ],
+      rows: [
+        doc: "Number of visible text lines for the control.",
+        type: :non_neg_integer,
+        default: 2
+      ],
+      readonly: [
+        doc: "Sets the field to readonly. Also see the [panels](/guides/fields/readonly.md) guide.",
+        type: {:or, [:boolean, {:fun, 1}]}
+      ]
+    ]
+
+    @moduledoc """
+    A field for handling long text values.
+
+    ## Field-specific options
+
+    See `Backpex.Field` for general field options.
+
+    #{NimbleOptions.docs(@config_schema)}
+    """
+    use Backpex.Field, config_schema: @config_schema
+
+    @impl Backpex.Field
+    def render_value(assigns) do
+      ~H"""
+      <p
+        class={[
+          @live_action in [:index, :resource_action] && "truncate",
+          @live_action == :show && "overflow-x-auto whitespace-pre-wrap"
+        ]}
+        phx-no-format
+      ><%= HTML.pretty_value(@value) %></p>
+      """
+    end
+
+    @impl Backpex.Field
+    def render_form(assigns) do
+      assigns |> dbg()
+      ~H"""
+      <div>
+        <Layout.field_container>
+          <:label align={Backpex.Field.align_label(@field_options, assigns, :top)}>
+            <Layout.input_label for={@form[@name]} text={@field_options[:label]} />
+          </:label>
+          <BackpexForm.input
+            type="textarea"
+            field={@form[@name]}
+            placeholder={@field_options[:placeholder]}
+            rows={@field_options[:rows]}
+            translate_error_fun={Backpex.Field.translate_error_fun(@field_options, assigns)}
+            help_text={Backpex.Field.help_text(@field_options, assigns)}
+            phx-debounce={Backpex.Field.debounce(@field_options, assigns)}
+            phx-throttle={Backpex.Field.throttle(@field_options, assigns)}
+            readonly={@readonly}
+            disabled={@readonly}
+          />
+        </Layout.field_container>
+      </div>
+      """
+    end
+  end
